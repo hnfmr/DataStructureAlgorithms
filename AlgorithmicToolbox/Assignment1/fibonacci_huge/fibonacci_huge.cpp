3a236c3e0924c90b98273521ab208820232f5f77
Wei Qi <qiwei.uk@gmail.com>
2016-04-13 23:53:57 +0800

    what a huge mess, resorted to use gcc 128bit int, needs cleaning up

--- AlgorithmicToolbox/Assignment1/fibonacci_huge/fibonacci_huge.cpp
+++ AlgorithmicToolbox/Assignment1/fibonacci_huge/fibonacci_huge.cpp
@@ -1,20 +1,53 @@
-#include <cstdint>
 #include <iostream>
+#include <stdio.h>
+#include <inttypes.h>
+#include <cstdint>
 
-std::uint64_t get_fibonaccihuge( std::uint64_t n, std::uint64_t m )
+/*
+ * ** Using documented GCC type unsigned __int128 instead of undocumented
+ * ** obsolescent typedef name __uint128_t.  Works with GCC 4.7.1 but not
+ * ** GCC 4.1.2 (but __uint128_t works with GCC 4.1.2) on Mac OS X 10.7.4.
+ * */
+typedef unsigned __int128 uint128_t;
+
+/*      UINT64_MAX 18446744073709551615ULL */
+#define P10_UINT64 10000000000000000000ULL /* 19 zeroes */
+#define E10_UINT64 19
+
+#define STRINGIZER( x ) #x
+#define TO_STRING( x ) STRINGIZER( x )
+
+static int print_u128_u( uint128_t u128 )
+{
+  int rc;
+  if ( u128 > UINT64_MAX ) {
+    uint128_t leading = u128 / P10_UINT64;
+    uint64_t trailing = u128 % P10_UINT64;
+    rc = print_u128_u( leading );
+    rc += printf( "%." TO_STRING( E10_UINT64 ) PRIu64, trailing );
+  }
+  else
+  {
+    uint64_t u64 = u128;
+    rc = printf( "%" PRIu64, u64 );
+  }
+  return rc;
+}
+
+uint128_t get_fibonaccihuge( uint128_t n, uint128_t m )
 {
   // write your code here
   return 0;
 }
 
-std::uint64_t calc_fib( std::uint64_t n )
+uint128_t calc_fib( uint128_t n )
 {
   if ( n <= 1 ) return n;
-  std::uint64_t a = 0;
-  std::uint64_t b = 1;
+  uint128_t a = 0;
+  uint128_t b = 1;
 
-  std::uint64_t c = 0;
-  for ( std::uint64_t i = 2; i <= n; ++i ) {
+  uint128_t c = 0;
+  for ( uint128_t i = 2; i <= n; ++i ) {
     c = a + b;
     a = b;
     b = c;
@@ -22,30 +55,30 @@ std::uint64_t calc_fib( std::uint64_t n )
   return c;
 }
 
-std::uint64_t pisano_period( std::uint64_t n, std::uint64_t q )
+uint128_t pisano_period( std::uint64_t n, std::uint64_t q )
 {
 
-  std::uint64_t a = 0;
-  std::uint64_t b = 1;
+  uint128_t a = 0;
+  uint128_t b = 1;
 
-  std::uint64_t c = 2;
+  uint128_t c = 2;
 
-  std::uint64_t r1 = 1;
-  std::uint64_t r = 0;
+  uint128_t m = 0;
 
-  std::uint64_t m = 0;
-
-  for ( std::uint64_t i = 2; i <= n; ++i ) {
+  for ( uint128_t i = 2; i <= n*n-1; ++i ) {
     c = a + b;
-    r = c % q;
+    /* r = c % q; */
     m = i - 1;
-    if ( r1 == 0 && r == 1 ) {
-      std::cout << "FOUND!\n";
+
+    if ( b == 0 && c == 1 ) {
+      std::cout << "Cycle: ";
+      print_u128_u(m);
+      std::cout << "\n";
       break;
     }
-    r1 = r;
-    a = b;
-    b = c;
+    /* r1 = r; */
+    a = b % q;
+    b = c % q;
   }
 
   return m;
@@ -53,15 +86,28 @@ std::uint64_t pisano_period( std::uint64_t n, std::uint64_t q )
 
 int main()
 {
-  //std::uint64_t n, m;
-  //std::cin >> n >> m;
-  // std::cout << get_fibonaccihuge( n, m ) << '\n';
+#if 0
+  for (int i = 0; i < 100; ++i) {
+    print_u128_u(calc_fib(i));
+    std::cout << "\n";
+  }
+#endif
+#if 1
+  std::uint64_t n, m;
+  std::cin >> n >> m;
+  //std::cout << get_fibonaccihuge( n, m ) << '\n';
 
-  //auto c = pisano_period( n, m );
-  //auto r = n % c;
-  std::uint64_t m = 30524;
-  auto r_fib = calc_fib( 22318 );
+  auto c = pisano_period( n, m );
+  auto r = n % c;
+
+  auto r_fib = calc_fib( r );
+  print_u128_u( r_fib );
+  std::cout << "\n";
+
   auto rr = r_fib % m;
+  print_u128_u( rr );
+  std::cout << "\n";
 
+#endif
   return 0;
 }
