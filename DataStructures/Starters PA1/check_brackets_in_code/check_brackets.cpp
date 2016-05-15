4cbb30935792d2c0b6cfd755b4776a1feecb93c0
Wei Qi <qiwei.uk@gmail.com>
2016-05-16 01:20:21 +0800

    still working on problem2 tree height

--- DataStructures/Starters PA1/check_brackets_in_code/check_brackets.cpp
+++ DataStructures/Starters PA1/check_brackets_in_code/check_brackets.cpp
@@ -2,44 +2,74 @@
 #include <stack>
 #include <string>
 
-struct Bracket {
-    Bracket(int type, int position):
-        type(type),
-        position(position)
-    {}
+struct Bracket
+{
+  Bracket( int type, int position ) : type( type ), position( position ) {}
 
-    bool Matchc(char c) {
-        if (type == '[' && c == ']')
-            return true;
-        if (type == '{' && c == '}')
-            return true;
-        if (type == '(' && c == ')')
-            return true;
-        return false;
-    }
+  bool Matchc( char c )
+  {
+    if ( type == '[' && c == ']' )
+      return true;
+    if ( type == '{' && c == '}' )
+      return true;
+    if ( type == '(' && c == ')' )
+      return true;
+    return false;
+  }
 
-    char type;
-    int position;
+  char type;
+  int position;
 };
 
-int main() {
-    std::string text;
-    getline(std::cin, text);
 
-    std::stack <Bracket> opening_brackets_stack;
-    for (int position = 0; position < text.length(); ++position) {
-        char next = text[position];
+int main()
+{
+  std::string text;
+  getline( std::cin, text );
 
-        if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
-        }
+  std::stack< Bracket > opening_brackets_stack;
 
-        if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
-        }
+  bool exit = false;
+  for ( size_t position = 0; position < text.length(); ++position )
+  {
+    char next = text[ position ];
+
+    if ( next == '(' || next == '[' || next == '{' )
+    {
+      opening_brackets_stack.push( Bracket( next, position ) );
+      // Process opening bracket, write your code here
     }
 
-    // Printing answer, write your code here
+    if ( next == ')' || next == ']' || next == '}' )
+    {
+      if (!opening_brackets_stack.empty()) {
+        auto left = opening_brackets_stack.top();
+        if (!left.Matchc(next)) {
+          std::cout << position+1 << "\n";
+          exit = true;
+          break;
+        }
+        else {
+          opening_brackets_stack.pop();
+        }
+      }
+      else {
+        std::cout << position+1 << "\n";
+        exit = true;
+        break;
+      }
+      
+      // Process closing bracket, write your code here
+    }
+  }
 
-    return 0;
+  // Printing answer, write your code here
+  if (opening_brackets_stack.empty() && !exit) {
+    std::cout << "Success\n";
+  }
+  else if (!exit) {
+    auto left = opening_brackets_stack.top();
+    std::cout << left.position+1 << "\n";
+  }
+  return 0;
 }
--- DataStructures/Starters PA1/tree_height/tree-height.cpp
+++ DataStructures/Starters PA1/tree_height/tree-height.cpp
@@ -1,34 +1,46 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-class TreeHeight {
-  int n;             
-  std::vector<int> parent;
+class TreeHeight
+{
+  int n;
+  std::vector< int > parent;
 
 public:
-  void read() {
+  void read()
+  {
     std::cin >> n;
-    parent.resize(n);
-    for (int i = 0; i < n; i++)
-      std::cin >> parent[i];
+    parent.resize( n );
+    for ( int i = 0; i < n; i++ )
+      std::cin >> parent[ i ];
   }
 
-  int compute_height() {
+  int compute_height()
+  {
     // Replace this code with a faster implementation
     int maxHeight = 0;
-    for (int vertex = 0; vertex < n; vertex++) {
-      int height = 0;
-      for (int i = vertex; i != -1; i = parent[i])
-        height++;
-      maxHeight = std::max(maxHeight, height);
+    auto it = std::lower_bound( parent.begin(), parent.end(), -1 );
+    int diff = 0;
+    if (it != parent.end()) {
+      diff = std::distance( parent.begin(), it );
     }
+    std::cout << "diff: " << diff << "\n";
+
+    /* for ( int vertex = 0; vertex < n; vertex++ ) */
+    /* { */
+    /*   int height = 0; */
+    /*   for ( int i = vertex; i != -1; i = parent[ i ] ) */
+    /*     height++; */
+    /*   maxHeight = std::max( maxHeight, height ); */
+    /* } */
     return maxHeight;
   }
 };
 
-int main() {
-  std::ios_base::sync_with_stdio(0);
+int main()
+{
+  std::ios_base::sync_with_stdio( 0 );
   TreeHeight tree;
   tree.read();
   std::cout << tree.compute_height() << std::endl;
