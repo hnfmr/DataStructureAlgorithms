7fd1cc6d56f566122896a7f0157721fe6def9f31
Wei Qi <qiwei.uk@gmail.com>
2016-05-16 02:32:51 -0700

    wip

--- DataStructures/Starters PA1/tree_height/tree-height.cpp
+++ DataStructures/Starters PA1/tree_height/tree-height.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <vector>
+//#include <stack>
 
 class TreeHeight
 {
@@ -19,22 +21,57 @@ public:
   int compute_height()
   {
     // Replace this code with a faster implementation
-    int maxHeight = 0;
-    auto it = std::lower_bound( parent.begin(), parent.end(), -1 );
-    int diff = 0;
-    if (it != parent.end()) {
-      diff = std::distance( parent.begin(), it );
-    }
-    std::cout << "diff: " << diff << "\n";
+    //int maxHeight = 0;
 
-    /* for ( int vertex = 0; vertex < n; vertex++ ) */
-    /* { */
-    /*   int height = 0; */
-    /*   for ( int i = vertex; i != -1; i = parent[ i ] ) */
-    /*     height++; */
-    /*   maxHeight = std::max( maxHeight, height ); */
-    /* } */
-    return maxHeight;
+    std::map< int, int > countMap;
+
+    //for (int i = 0; i < parent.size(); ++i)
+    //{
+    //  if (parent[i] == -1)
+    //    countMap[i] = 1;
+    //  else
+    //    countMap[i] = 0;
+    //}
+
+    int maxHeight = 0;
+    int height = 0;
+    for ( int i = 0; i < n; ++i )
+    {
+      int n = parent[ i ];
+      if ( n == -1 )
+        continue;
+
+      int c = 0;
+      int j = n;
+
+      if ( countMap.find( parent[ n ] ) != countMap.end() )
+      {
+        height = countMap[ parent[ n ] ] + 1;
+        //countMap[ parent[ n ] ] = height;
+      }
+      else
+      {
+        while ( parent[ j ] != -1 )
+        {
+          j = parent[ j ];
+          ++c;
+        }
+        height = ++c;
+        countMap[ n ] = height;
+      }
+
+      if ( height > maxHeight )
+        maxHeight = height;
+    }
+
+    //for ( int vertex = 0; vertex < n; vertex++ )
+    //{
+    //  int height = 0;
+    //  for ( int i = vertex; i != -1; i = parent[ i ] )
+    //    height++;
+    //  maxHeight = std::max( maxHeight, height );
+    //}
+    return maxHeight+1;
   }
 };
 
@@ -44,4 +81,6 @@ int main()
   TreeHeight tree;
   tree.read();
   std::cout << tree.compute_height() << std::endl;
+
+  return 0;
 }
