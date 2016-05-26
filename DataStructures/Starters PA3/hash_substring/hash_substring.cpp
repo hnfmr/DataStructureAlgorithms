ac71b3f2e55d1a946e9eee4ac80543f10b2b7fe4
Wei Qi <qiwei.uk@gmail.com>
2016-05-26 01:23:38 -0700

    Added slides

--- DataStructures/Starters PA3/hash_substring/hash_substring.cpp
+++ DataStructures/Starters PA3/hash_substring/hash_substring.cpp
@@ -5,34 +5,38 @@
 using std::string;
 typedef unsigned long long ull;
 
-struct Data {
-    string pattern, text;
+struct Data
+{
+  string pattern, text;
 };
 
-Data read_input() {
-    Data data;
-    std::cin >> data.pattern >> data.text;
-    return data;
+Data read_input()
+{
+  Data data;
+  std::cin >> data.pattern >> data.text;
+  return data;
 }
 
-void print_occurrences(const std::vector<int>& output) {
-    for (size_t i = 0; i < output.size(); ++i)
-        std::cout << output[i] << " ";
-    std::cout << "\n";
+void print_occurrences( const std::vector< int > &output )
+{
+  for ( size_t i = 0; i < output.size(); ++i )
+    std::cout << output[ i ] << " ";
+  std::cout << "\n";
 }
 
-std::vector<int> get_occurrences(const Data& input) {
-    const string& s = input.pattern, t = input.text;
-    std::vector<int> ans;
-    for (size_t i = 0; i + s.size() <= t.size(); ++i)
-        if (t.substr(i, s.size()) == s)
-            ans.push_back(i);
-    return ans;
+std::vector< int > get_occurrences( const Data &input )
+{
+  const string &s = input.pattern, t = input.text;
+  std::vector< int > ans;
+  for ( size_t i = 0; i + s.size() <= t.size(); ++i )
+    if ( t.substr( i, s.size() ) == s )
+      ans.push_back( i );
+  return ans;
 }
 
-
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    print_occurrences(get_occurrences(read_input()));
-    return 0;
+int main()
+{
+  std::ios_base::sync_with_stdio( false );
+  print_occurrences( get_occurrences( read_input() ) );
+  return 0;
 }
