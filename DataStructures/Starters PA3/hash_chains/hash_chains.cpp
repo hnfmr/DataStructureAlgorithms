9b255ba2594cc6b1b01ad2281740c56918137bcc
Wei Qi <qiwei.uk@gmail.com>
2016-05-28 22:45:36 +0800

    week3 wip

--- DataStructures/Starters PA3/hash_chains/hash_chains.cpp
+++ DataStructures/Starters PA3/hash_chains/hash_chains.cpp
@@ -7,32 +7,26 @@ using std::string;
 using std::vector;
 using std::cin;
 
-struct Query
-{
+struct Query {
   string type, s;
   size_t ind;
 };
 
-class QueryProcessor
-{
+class QueryProcessor {
   int bucket_count;
   // store all strings in one vector
   vector< string > elems;
-  size_t hash_func( const string &s ) const
+  size_t hash_func( const string& s ) const
   {
     static const size_t multiplier = 263;
     static const size_t prime = 1000000007;
     unsigned long long hash = 0;
-    for ( int i = static_cast< int >( s.size() ) - 1; i >= 0; --i )
-      hash = ( hash * multiplier + s[ i ] ) % prime;
+    for ( int i = static_cast< int >( s.size() ) - 1; i >= 0; --i ) hash = ( hash * multiplier + s[i] ) % prime;
     return hash % bucket_count;
   }
 
 public:
-  explicit QueryProcessor( int bucket_count )
-    : bucket_count( bucket_count )
-  {
-  }
+  explicit QueryProcessor( int bucket_count ) : bucket_count( bucket_count ) {}
 
   Query readQuery() const
   {
@@ -45,36 +39,25 @@ public:
     return query;
   }
 
-  void writeSearchResult( bool was_found ) const
-  {
-    std::cout << ( was_found ? "yes\n" : "no\n" );
-  }
+  void writeSearchResult( bool was_found ) const { std::cout << ( was_found ? "yes\n" : "no\n" ); }
 
-  void processQuery( const Query &query )
+  void processQuery( const Query& query )
   {
-    if ( query.type == "check" )
-    {
+    if ( query.type == "check" ) {
       // use reverse order, because we append strings to the end
       for ( int i = static_cast< int >( elems.size() ) - 1; i >= 0; --i )
-        if ( hash_func( elems[ i ] ) == query.ind )
-          std::cout << elems[ i ] << " ";
+        if ( hash_func( elems[i] ) == query.ind ) std::cout << elems[i] << " ";
       std::cout << "\n";
     }
-    else
-    {
-      vector< string >::iterator it =
-        std::find( elems.begin(), elems.end(), query.s );
+    else {
+      vector< string >::iterator it = std::find( elems.begin(), elems.end(), query.s );
       if ( query.type == "find" )
         writeSearchResult( it != elems.end() );
-      else if ( query.type == "add" )
-      {
-        if ( it == elems.end() )
-          elems.push_back( query.s );
+      else if ( query.type == "add" ) {
+        if ( it == elems.end() ) elems.push_back( query.s );
       }
-      else if ( query.type == "del" )
-      {
-        if ( it != elems.end() )
-          elems.erase( it );
+      else if ( query.type == "del" ) {
+        if ( it != elems.end() ) elems.erase( it );
       }
     }
   }
@@ -83,8 +66,7 @@ public:
   {
     int n;
     cin >> n;
-    for ( int i = 0; i < n; ++i )
-      processQuery( readQuery() );
+    for ( int i = 0; i < n; ++i ) processQuery( readQuery() );
   }
 };
 
--- DataStructures/Starters PA3/phone_book/phone_book.cpp
+++ DataStructures/Starters PA3/phone_book/phone_book.cpp
@@ -1,4 +1,8 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -6,8 +10,57 @@ using std::string;
 using std::vector;
 using std::cin;
 
-struct Query
-{
+template < size_t N, size_t L = 16 > class Map {
+public:
+  Map() : data_( N, nullptr ) {}
+  ~Map()
+  {
+    for ( size_t i = 0; i < data_.size(); ++i ) {
+      if ( auto addr = data_[i] ) delete[] addr;
+    }
+  }
+
+  void insert( int key, const std::string& value )
+  {
+    char* b = data_[key];
+
+    if ( b == nullptr ) {
+      b = new char[L];
+    }
+
+    std::memset( b, '\0', L );
+    for ( size_t i = 0; i < value.size(); ++i ) {
+      b[i] = value[i];
+    }
+
+    data_[key] = b;
+  }
+
+  std::string find( int key )
+  {
+    char* b = data_[key];
+    if ( b == nullptr )
+      return "not found";
+    else
+      return std::string( b );
+  }
+
+  void del( int key )
+  {
+    char* b = data_[key];
+    if ( b != nullptr ) {
+      delete[] b;
+      b = nullptr;
+    }
+    data_[key] = nullptr;
+  }
+  // void insert( int key, const std::string& value ) { data_[key] = value; }
+
+private:
+  std::vector< char* > data_;
+};
+
+struct Query {
   string type, name;
   int number;
 };
@@ -17,70 +70,69 @@ vector< Query > read_queries()
   int n;
   cin >> n;
   vector< Query > queries( n );
-  for ( int i = 0; i < n; ++i )
-  {
-    cin >> queries[ i ].type;
-    if ( queries[ i ].type == "add" )
-      cin >> queries[ i ].number >> queries[ i ].name;
+  for ( int i = 0; i < n; ++i ) {
+    cin >> queries[i].type;
+    if ( queries[i].type == "add" )
+      cin >> queries[i].number >> queries[i].name;
     else
-      cin >> queries[ i ].number;
+      cin >> queries[i].number;
   }
   return queries;
 }
 
-void write_responses( const vector< string > &result )
+void write_responses( const vector< string >& result )
 {
-  for ( size_t i = 0; i < result.size(); ++i )
-    std::cout << result[ i ] << "\n";
+  for ( size_t i = 0; i < result.size(); ++i ) std::cout << result[i] << "\n";
 }
 
-vector< string > process_queries( const vector< Query > &queries )
+vector< string > p_queries( const vector< Query >& qs )
+{
+  vector< string > result;
+  std::map< int, std::string > m;
+  for ( size_t i = 0; i < qs.size(); ++i ) {
+    const auto& q = qs[i];
+    if ( q.type == "add" ) {
+      m[q.number] = q.name;
+    }
+    else if ( q.type == "del" ) {
+      m.erase( q.number );
+    }
+    else {
+      if ( m.find( q.number ) != m.end() ) {
+        result.push_back( m[q.number] );
+      }
+      else {
+        result.emplace_back( "not found" );
+      }
+    }
+  }
+  return result;
+}
+vector< string > process_queries( const vector< Query >& queries )
 {
   vector< string > result;
   // Keep list of all existing (i.e. not deleted yet) contacts.
-  vector< Query > contacts;
-  for ( size_t i = 0; i < queries.size(); ++i )
-    if ( queries[ i ].type == "add" )
-    {
-      bool was_founded = false;
-      // if we already have contact with such number,
-      // we should rewrite contact's name
-      for ( size_t j = 0; j < contacts.size(); ++j )
-        if ( contacts[ j ].number == queries[ i ].number )
-        {
-          contacts[ j ].name = queries[ i ].name;
-          was_founded = true;
-          break;
-        }
-      // otherwise, just add it
-      if ( !was_founded )
-        contacts.push_back( queries[ i ] );
+  // vector< Query > contacts;
+  Map< 10000000 > m;
+  for ( size_t i = 0; i < queries.size(); ++i ) {
+    const auto& q = queries[i];
+    if ( q.type == "add" ) {
+      m.insert( q.number, q.name );
     }
-    else if ( queries[ i ].type == "del" )
-    {
-      for ( size_t j = 0; j < contacts.size(); ++j )
-        if ( contacts[ j ].number == queries[ i ].number )
-        {
-          contacts.erase( contacts.begin() + j );
-          break;
-        }
+    else if ( q.type == "del" ) {
+      m.del( q.number );
     }
-    else
-    {
-      string response = "not found";
-      for ( size_t j = 0; j < contacts.size(); ++j )
-        if ( contacts[ j ].number == queries[ i ].number )
-        {
-          response = contacts[ j ].name;
-          break;
-        }
-      result.push_back( response );
+    else {
+      result.push_back( m.find( q.number ) );
     }
+  }
   return result;
 }
 
 int main()
 {
+  // write_responses(p_queries(read_queries()));
+
   write_responses( process_queries( read_queries() ) );
   return 0;
 }
