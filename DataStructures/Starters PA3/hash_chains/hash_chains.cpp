e262092a45c8ed6ee9d7b6e52f71afe12171a1bb
Wei Qi <qiwei.uk@gmail.com>
2016-05-28 23:52:58 +0800

    week3 problem2

--- DataStructures/Starters PA3/hash_chains/hash_chains.cpp
+++ DataStructures/Starters PA3/hash_chains/hash_chains.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <deque>
+#include <sstream>
 
 using std::string;
 using std::vector;
@@ -12,10 +14,11 @@ struct Query {
   size_t ind;
 };
 
+
 class QueryProcessor {
   int bucket_count;
-  // store all strings in one vector
-  vector< string > elems;
+  std::vector< std::deque< std::string > > data_;
+  std::vector<std::string> results;
   size_t hash_func( const string& s ) const
   {
     static const size_t multiplier = 263;
@@ -26,7 +29,12 @@ class QueryProcessor {
   }
 
 public:
-  explicit QueryProcessor( int bucket_count ) : bucket_count( bucket_count ) {}
+  explicit QueryProcessor( int bucket_count )
+    : bucket_count( bucket_count )
+    , data_( bucket_count )
+    , results()
+  {
+  }
 
   Query readQuery() const
   {
@@ -39,25 +47,76 @@ public:
     return query;
   }
 
-  void writeSearchResult( bool was_found ) const { std::cout << ( was_found ? "yes\n" : "no\n" ); }
+  void writeSearchResult( bool was_found )
+  {
+    std::string result = was_found ? "yes" : "no";
+    results.push_back( result );
+  }
+
+  void insert( const std::string &str )
+  {
+    int bc = hash_func( str );
+    auto &q = data_[ bc ];
+    if ( std::find( q.begin(), q.end(), str ) == q.end() )
+      q.push_front( str );
+  }
+
+  std::string check( int ind )
+  {
+    std::stringstream ss;
+    const auto &q = data_[ ind ];
+    if ( q.empty() )
+    {
+      ss << "";
+      return ss.str();
+    }
+
+    for ( auto it = q.begin(); it != q.end(); ++it )
+    {
+      ss << *it << " ";
+    }
+    return ss.str();
+    ;
+  }
+
+  bool find( const std::string &str )
+  {
+    int bc = hash_func( str );
+    const auto &q = data_[ bc ];
+    auto it = std::find( q.begin(), q.end(), str );
+    return it != q.end();
+  }
+
+  void del( const std::string &str )
+  {
+    int bc = hash_func( str );
+    auto &q = data_[ bc ];
+    auto it = std::find( q.begin(), q.end(), str );
+    if ( it != q.end() )
+      q.erase( it );
+  }
 
   void processQuery( const Query& query )
   {
     if ( query.type == "check" ) {
       // use reverse order, because we append strings to the end
-      for ( int i = static_cast< int >( elems.size() ) - 1; i >= 0; --i )
-        if ( hash_func( elems[i] ) == query.ind ) std::cout << elems[i] << " ";
-      std::cout << "\n";
+      /* for ( int i = static_cast< int >( elems.size() ) - 1; i >= 0; --i ) */
+      /*   if ( hash_func( elems[i] ) == query.ind ) std::cout << elems[i] << " "; */
+      auto es = check( query.ind );
+      results.push_back( es );
     }
     else {
-      vector< string >::iterator it = std::find( elems.begin(), elems.end(), query.s );
+      /* vector< string >::iterator it = std::find( elems.begin(), elems.end(), query.s ); */
       if ( query.type == "find" )
-        writeSearchResult( it != elems.end() );
+        /* writeSearchResult( it != elems.end() ); */
+        writeSearchResult( find( query.s ) );
       else if ( query.type == "add" ) {
-        if ( it == elems.end() ) elems.push_back( query.s );
+        insert( query.s );
+        /* if ( it == elems.end() ) elems.push_back( query.s ); */
       }
       else if ( query.type == "del" ) {
-        if ( it != elems.end() ) elems.erase( it );
+        /* if ( it != elems.end() ) elems.erase( it ); */
+        del( query.s );
       }
     }
   }
@@ -67,6 +126,10 @@ public:
     int n;
     cin >> n;
     for ( int i = 0; i < n; ++i ) processQuery( readQuery() );
+
+    for (const auto& r : results) {
+      std::cout << r << "\n";
+    }
   }
 };
 
