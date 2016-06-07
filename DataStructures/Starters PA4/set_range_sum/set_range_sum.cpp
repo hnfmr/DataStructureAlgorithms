0e4e1c3001f0ce398fa66af5a296de7c0c94f8b5
Wei Qi <qiwei.uk@gmail.com>
2016-06-07 02:31:39 -0700

    last week wip

--- DataStructures/Starters PA4/set_range_sum/set_range_sum.cpp
+++ DataStructures/Starters PA4/set_range_sum/set_range_sum.cpp
@@ -3,83 +3,114 @@
 // Splay tree implementation
 
 // Vertex of a splay tree
-struct Vertex {
+struct Vertex
+{
   int key;
   // Sum of all the keys in the subtree - remember to update
   // it after each operation that changes the tree.
   long long sum;
-  Vertex* left;
-  Vertex* right;
-  Vertex* parent;
+  Vertex *left;
+  Vertex *right;
+  Vertex *parent;
 
-  Vertex(int key, long long sum, Vertex* left, Vertex* right, Vertex* parent) 
-  : key(key), sum(sum), left(left), right(right), parent(parent) {}
+  Vertex( int key, long long sum, Vertex *left, Vertex *right, Vertex *parent )
+    : key( key )
+    , sum( sum )
+    , left( left )
+    , right( right )
+    , parent( parent )
+  {
+  }
 };
 
-void update(Vertex* v) {
-  if (v == NULL) return;
-  v->sum = v->key + (v->left != NULL ? v->left->sum : 0ll) + (v->right != NULL ? v->right->sum : 0ll);
-  if (v->left != NULL) {
+void update( Vertex *v )
+{
+  if ( v == NULL )
+    return;
+  v->sum = v->key + ( v->left != NULL ? v->left->sum : 0ll ) +
+           ( v->right != NULL ? v->right->sum : 0ll );
+  if ( v->left != NULL )
+  {
     v->left->parent = v;
   }
-  if (v->right != NULL) {
+  if ( v->right != NULL )
+  {
     v->right->parent = v;
   }
 }
 
-void small_rotation(Vertex* v) {
-  Vertex* parent = v->parent;
-  if (parent == NULL) {
+void small_rotation( Vertex *v )
+{
+  Vertex *parent = v->parent;
+  if ( parent == NULL )
+  {
     return;
   }
-  Vertex* grandparent = v->parent->parent;
-  if (parent->left == v) {
-    Vertex* m = v->right;
+  Vertex *grandparent = v->parent->parent;
+  if ( parent->left == v )
+  {
+    Vertex *m = v->right;
     v->right = parent;
     parent->left = m;
-  } else {
-    Vertex* m = v->left;
+  }
+  else
+  {
+    Vertex *m = v->left;
     v->left = parent;
     parent->right = m;
   }
-  update(parent);
-  update(v);
+  update( parent );
+  update( v );
   v->parent = grandparent;
-  if (grandparent != NULL) {
-    if (grandparent->left == parent) {
+  if ( grandparent != NULL )
+  {
+    if ( grandparent->left == parent )
+    {
       grandparent->left = v;
-    } else {
+    }
+    else
+    {
       grandparent->right = v;
     }
   }
 }
 
-void big_rotation(Vertex* v) {
-  if (v->parent->left == v && v->parent->parent->left == v->parent) {
+void big_rotation( Vertex *v )
+{
+  if ( v->parent->left == v && v->parent->parent->left == v->parent )
+  {
     // Zig-zig
-    small_rotation(v->parent);
-    small_rotation(v);
-  } else if (v->parent->right == v && v->parent->parent->right == v->parent) {
+    small_rotation( v->parent );
+    small_rotation( v );
+  }
+  else if ( v->parent->right == v && v->parent->parent->right == v->parent )
+  {
     // Zig-zig
-    small_rotation(v->parent);
-    small_rotation(v);
-  } else {
+    small_rotation( v->parent );
+    small_rotation( v );
+  }
+  else
+  {
     // Zig-zag
-    small_rotation(v);
-    small_rotation(v);
-  }  
+    small_rotation( v );
+    small_rotation( v );
+  }
 }
 
 // Makes splay of the given vertex and makes
 // it the new root.
-void splay(Vertex*& root, Vertex* v) {
-  if (v == NULL) return;
-  while (v->parent != NULL) {
-    if (v->parent->parent == NULL) {
-      small_rotation(v);
+void splay( Vertex *&root, Vertex *v )
+{
+  if ( v == NULL )
+    return;
+  while ( v->parent != NULL )
+  {
+    if ( v->parent->parent == NULL )
+    {
+      small_rotation( v );
       break;
     }
-    big_rotation(v);
+    big_rotation( v );
   }
   root = v;
 }
@@ -89,130 +120,160 @@ void splay(Vertex*& root, Vertex* v) {
 // If found, returns a pointer to the node with the given key.
 // Otherwise, returns a pointer to the node with the smallest
 // bigger key (next value in the order).
-// If the key is bigger than all keys in the tree, 
+// If the key is bigger than all keys in the tree,
 // returns NULL.
-Vertex* find(Vertex*& root, int key) {
-  Vertex* v = root;
-  Vertex* last = root;
-  Vertex* next = NULL;
-  while (v != NULL) {
-    if (v->key >= key && (next == NULL || v->key < next->key)) {
+Vertex *find( Vertex *&root, int key )
+{
+  Vertex *v = root;
+  Vertex *last = root;
+  Vertex *next = NULL;
+  while ( v != NULL )
+  {
+    if ( v->key >= key && ( next == NULL || v->key < next->key ) )
+    {
       next = v;
     }
     last = v;
-    if (v->key == key) {
-      break;      
+    if ( v->key == key )
+    {
+      break;
     }
-    if (v->key < key) {
+    if ( v->key < key )
+    {
       v = v->right;
-    } else {
+    }
+    else
+    {
       v = v->left;
     }
   }
-  splay(root, last);
+  splay( root, last );
   return next;
 }
 
-void split(Vertex* root, int key, Vertex*& left, Vertex*& right) {
-  right = find(root, key);
-  splay(root, right);
-  if (right == NULL) {
+void split( Vertex *root, int key, Vertex *&left, Vertex *&right )
+{
+  right = find( root, key );
+  splay( root, right );
+  if ( right == NULL )
+  {
     left = root;
     return;
   }
   left = right->left;
   right->left = NULL;
-  if (left != NULL) {
+  if ( left != NULL )
+  {
     left->parent = NULL;
   }
-  update(left);
-  update(right);
+  update( left );
+  update( right );
 }
 
-Vertex* merge(Vertex* left, Vertex* right) {
-  if (left == NULL) return right;
-  if (right == NULL) return left;
-  Vertex* min_right = right;
-  while (min_right->left != NULL) {
+Vertex *merge( Vertex *left, Vertex *right )
+{
+  if ( left == NULL )
+    return right;
+  if ( right == NULL )
+    return left;
+  Vertex *min_right = right;
+  while ( min_right->left != NULL )
+  {
     min_right = min_right->left;
   }
-  splay(right, min_right);
+  splay( right, min_right );
   right->left = left;
-  update(right);
+  update( right );
   return right;
 }
 
 // Code that uses splay tree to solve the problem
 
-Vertex* root = NULL;
+Vertex *root = NULL;
 
-void insert(int x) {
-  Vertex* left = NULL;
-  Vertex* right = NULL;
-  Vertex* new_vertex = NULL;  
-  split(root, x, left, right);
-  if (right == NULL || right->key != x) {
-    new_vertex = new Vertex(x, x, NULL, NULL, NULL);
+void insert( int x )
+{
+  Vertex *left = NULL;
+  Vertex *right = NULL;
+  Vertex *new_vertex = NULL;
+  split( root, x, left, right );
+  if ( right == NULL || right->key != x )
+  {
+    new_vertex = new Vertex( x, x, NULL, NULL, NULL );
   }
-  root = merge(merge(left, new_vertex), right);
+  root = merge( merge( left, new_vertex ), right );
 }
 
-void erase(int x) {                   
+void erase( int x )
+{
   // Implement erase yourself
-
 }
 
-bool find(int x) {  
+bool find( int x )
+{
   // Implement find yourself
 
   return false;
 }
 
-long long sum(int from, int to) {
-  Vertex* left = NULL;
-  Vertex* middle = NULL;
-  Vertex* right = NULL;
-  split(root, from, left, middle);
-  split(middle, to + 1, middle, right);
+long long sum( int from, int to )
+{
+  Vertex *left = NULL;
+  Vertex *middle = NULL;
+  Vertex *right = NULL;
+  split( root, from, left, middle );
+  split( middle, to + 1, middle, right );
   long long ans = 0;
   // Complete the implementation of sum
-  
-  return ans;  
+
+  return ans;
 }
 
 const int MODULO = 1000000001;
 
-int main(){
+int main()
+{
   int n;
-  scanf("%d", &n);
+  scanf( "%d", &n );
   int last_sum_result = 0;
-  for (int i = 0; i < n; i++) {
-    char buffer[10];
-    scanf("%s", buffer);
-    char type = buffer[0];
-    switch (type) {
-      case '+' : {
-        int x;
-        scanf("%d", &x);
-        insert((x + last_sum_result) % MODULO);
-      } break;
-      case '-' : {
-        int x;
-        scanf("%d", &x);
-        erase((x + last_sum_result) % MODULO);
-      } break;            
-      case '?' : {
-        int x;
-        scanf("%d", &x);
-        printf(find((x + last_sum_result) % MODULO) ? "Found\n" : "Not found\n");
-      } break;
-      case 's' : {
-        int l, r;
-        scanf("%d %d", &l, &r);
-        long long res = sum((l + last_sum_result) % MODULO, (r + last_sum_result) % MODULO);
-        printf("%lld\n", res);
-        last_sum_result = int(res % MODULO);
-      }
+  for ( int i = 0; i < n; i++ )
+  {
+    char buffer[ 10 ];
+    scanf( "%s", buffer );
+    char type = buffer[ 0 ];
+    switch ( type )
+    {
+    case '+':
+    {
+      int x;
+      scanf( "%d", &x );
+      insert( ( x + last_sum_result ) % MODULO );
+    }
+    break;
+    case '-':
+    {
+      int x;
+      scanf( "%d", &x );
+      erase( ( x + last_sum_result ) % MODULO );
+    }
+    break;
+    case '?':
+    {
+      int x;
+      scanf( "%d", &x );
+      printf( find( ( x + last_sum_result ) % MODULO ) ? "Found\n"
+                                                       : "Not found\n" );
+    }
+    break;
+    case 's':
+    {
+      int l, r;
+      scanf( "%d %d", &l, &r );
+      long long res = sum( ( l + last_sum_result ) % MODULO,
+                           ( r + last_sum_result ) % MODULO );
+      printf( "%lld\n", res );
+      last_sum_result = int( res % MODULO );
+    }
     }
   }
   return 0;
--- DataStructures/Starters PA4/tree_orders/tree-orders.cpp
+++ DataStructures/Starters PA4/tree_orders/tree-orders.cpp
@@ -1,71 +1,125 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <iostream>
+#include <stack>
+#include <vector>
 
 using std::vector;
 using std::ios_base;
 using std::cin;
 using std::cout;
 
-class TreeOrders {
+class TreeOrders
+{
   int n;
-  vector <int> key;
-  vector <int> left;
-  vector <int> right;
+  vector< int > key;
+  vector< int > left;
+  vector< int > right;
 
 public:
-  void read() {
+  void read()
+  {
     cin >> n;
-    key.resize(n);
-    left.resize(n);
-    right.resize(n);
-    for (int i = 0; i < n; i++) {
-      cin >> key[i] >> left[i] >> right[i];
+    key.resize( n );
+    left.resize( n );
+    right.resize( n );
+    for ( int i = 0; i < n; i++ )
+    {
+      cin >> key[ i ] >> left[ i ] >> right[ i ];
     }
   }
 
+  struct Node
+  {
+    int key;
+    int left;
+    int right;
+  };
 
-  vector <int> in_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
+  void inorder_traverse( std::vector< int > &result, int nodeIdx )
+  {
+    if ( nodeIdx == -1 )
+      return;
 
+    int k = key[ nodeIdx ];
+    int l = left[ nodeIdx ];
+    int r = right[ nodeIdx ];
+
+    inorder_traverse( result, l );
+    result.push_back( k );
+    inorder_traverse( result, r );
+  }
+
+  vector< int > in_order()
+  {
+    if ( key.empty() )
+      return std::vector< int >();
+
+    vector< int > result;
+    inorder_traverse( result, 0 );
     return result;
   }
 
-  vector <int> pre_order() {
-    vector<int> result;    
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
+  void preorder_traverse( std::vector< int > &result, int nodeIdx )
+  {
+    if ( nodeIdx == -1 )
+      return;
+
+    result.push_back( key[ nodeIdx ] );
+    preorder_traverse( result, left[ nodeIdx ] );
+    preorder_traverse( result, right[ nodeIdx ] );
+  }
+
+  vector< int > pre_order()
+  {
+    if ( key.empty() )
+      return std::vector< int >();
+
+    vector< int > result;
+    preorder_traverse( result, 0 );
     return result;
   }
 
-  vector <int> post_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
+  void postorder_traverse( std::vector< int > &result, int nodeIdx )
+  {
+    if ( nodeIdx == -1 )
+      return;
+
+    postorder_traverse( result, left[ nodeIdx ] );
+    postorder_traverse( result, right[ nodeIdx ] );
+
+    result.push_back( key[ nodeIdx ] );
+  }
+  vector< int > post_order()
+  {
+    if (key.empty())
+      return std::vector< int >();
+
+    vector< int > result;
+    postorder_traverse( result, 0 );
     return result;
   }
 };
 
-void print(vector <int> a) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (i > 0) {
+void print( vector< int > a )
+{
+  for ( size_t i = 0; i < a.size(); i++ )
+  {
+    if ( i > 0 )
+    {
       cout << ' ';
     }
-    cout << a[i];
+    cout << a[ i ];
   }
   cout << '\n';
 }
 
-int main() {
-  ios_base::sync_with_stdio(0);
+int main()
+{
+  ios_base::sync_with_stdio( 0 );
   TreeOrders t;
   t.read();
-  print(t.in_order());
-  print(t.pre_order());
-  print(t.post_order());
+  print( t.in_order() );
+  print( t.pre_order() );
+  print( t.post_order() );
   return 0;
 }
