f991972b533089ced3426515ba4f4a2501b1c6a7
Wei Qi <qiwei.uk@gmail.com>
2016-06-12 00:27:11 +0800

    Done Graphs week 1 assignments

--- AlgorithmsOnGraphs/connected_components/connected_components.cpp
+++ AlgorithmsOnGraphs/connected_components/connected_components.cpp
@@ -1,24 +1,58 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using std::vector;
 using std::pair;
 
-int number_of_components(vector<vector<int> > &adj) {
+void explore( const vector< vector< int > > &adj, vector< bool > &visited,
+              std::vector< int > &ccnum, int v, int cc )
+{
+  visited[ v ] = true;
+  ccnum[ v ] = cc;
+  const auto &es = adj[ v ];
+  for ( const auto &w : es )
+  {
+    if ( !visited[ w ] )
+    {
+      explore( adj, visited, ccnum, w, cc );
+    }
+  }
+}
+
+int number_of_components( vector< vector< int > > &adj )
+{
   int res = 0;
-  //write your code here
+  // write your code here
+  std::vector< bool > visited( adj.size(), false );
+  std::vector< int > ccnum( adj.size(), 0 );
+  int cc = 1;
+
+  for ( int v = 0; v < adj.size(); ++v )
+  {
+    if ( !visited[ v ] )
+    {
+      explore( adj, visited, ccnum, v, cc );
+      ++cc;
+    }
+  }
+
+  std::sort( ccnum.begin(), ccnum.end() );
+  res = std::unique( ccnum.begin(), ccnum.end() ) - ccnum.begin();
   return res;
 }
 
-int main() {
+int main()
+{
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
+  vector< vector< int > > adj( n, vector< int >() );
+  for ( size_t i = 0; i < m; i++ )
+  {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[ x - 1 ].push_back( y - 1 );
+    adj[ y - 1 ].push_back( x - 1 );
   }
-  std::cout << number_of_components(adj);
+  std::cout << number_of_components( adj );
 }
--- AlgorithmsOnGraphs/reachability/reachability.cpp
+++ AlgorithmsOnGraphs/reachability/reachability.cpp
@@ -4,23 +4,47 @@
 using std::vector;
 using std::pair;
 
+void explore( const vector< vector< int > > &adj, vector< bool > &visited,
+              int v )
+{
+  visited[ v ] = true;
+  const auto &es = adj[ v ];
 
-int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
+  for ( const auto &w : es )
+  {
+
+    if ( !visited[ w ] )
+    {
+      explore( adj, visited, w );
+    }
+  }
+}
+int reach( vector< vector< int > > &adj, int x, int y )
+{
+  // write your code here
+  std::vector< bool > visited( adj.size(), false );
+
+  explore( adj, visited, x );
+
+  if ( visited[ y ] )
+    return 1;
+
   return 0;
 }
 
-int main() {
+int main()
+{
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
+  vector< vector< int > > adj( n, vector< int >() );
+  for ( size_t i = 0; i < m; i++ )
+  {
     int x, y;
     std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
+    adj[ x - 1 ].push_back( y - 1 );
+    adj[ y - 1 ].push_back( x - 1 );
   }
   int x, y;
   std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+  std::cout << reach( adj, x - 1, y - 1 );
 }
