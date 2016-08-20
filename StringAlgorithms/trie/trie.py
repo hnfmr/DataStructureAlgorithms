db07c46e8960416ad1fad29d65582c6095cc0315
Wei Qi <qiwei.uk@gmail.com>
2016-08-20 11:31:03 +0800

    wip

--- StringAlgorithms/trie/trie.py
+++ StringAlgorithms/trie/trie.py
@@ -1,5 +1,14 @@
-#Uses python3
+# Uses python3
 import sys
+
+
+def outgoing_edge(tree, current_node, symbol):
+    edges = tree.get(current_node)
+    if edges != None:
+        return edges.get(symbol)
+    else:
+        return None
+
 
 # Return the trie built from patterns
 # in the form of a dictionary of dictionaries,
@@ -12,6 +21,24 @@ import sys
 def build_trie(patterns):
     tree = dict()
     # write your code here
+    root = 0
+    tree[root] = dict()
+    max_node = 0
+    for pattern in patterns:
+        current_node = root
+        for current_symbol in pattern:
+            edge = outgoing_edge(tree, current_node, current_symbol)
+            if edge != None:
+                current_node = tree[current_node][current_symbol]
+            else:
+                symbols = tree.get(current_node)
+                if symbols != None:
+                    symbols[current_symbol] = max_node + 1
+                else:
+                    tree[current_node] = dict()
+                    tree[current_node][current_symbol] = max_node + 1
+                current_node = max_node + 1
+                max_node = current_node
     return tree
 
 
